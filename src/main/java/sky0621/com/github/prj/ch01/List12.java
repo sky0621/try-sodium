cec0c160782da73ded13c06b3d1d877927528c2d
sky0621 <sky14work@gmail.com>
2017-09-12 01:49:02 +0900

    init

---
+++ src/main/java/sky0621/com/github/prj/ch01/List12.java
@@ -0,0 +1,7 @@
+package sky0621.com.github.prj.ch01;
+
+public class List12 {
+	public void sample() {
+		SDateField dep = new SDateField();
+	}
+}
---
+++ src/main/java/sky0621/com/github/prj/ch01/ListBox.java
@@ -0,0 +1,26 @@
+package sky0621.com.github.prj.ch01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListBox {
+	public interface Listener {
+		void itemSelected(int index);
+	}
+
+	private List<Listener> listeners = new ArrayList<>();
+
+	public void addListener(Listener l) {
+		listeners.add(l);
+	}
+
+	public void removeListener(Listener l) {
+		listeners.remove(l);
+	}
+
+	protected void notifyItemSelected(int index) {
+//		for (l : listeners) {
+//			l.itemSelected(index)
+//		}
+	}
+}
